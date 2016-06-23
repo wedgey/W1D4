1c8bc654ab52d4ad214f9357a7f2af0bf29ad897
wedgey <desmondpoon5@hotmail.com>
2016-06-23 20:55:18 +0000

    Initial commit of candidates

--- candidates/filters.rb
+++ candidates/filters.rb
@@ -2,10 +2,42 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-
+	@candidates.detect { |candidate| candidate[:id] == id }
 end
 
 def experienced?(candidate)
+	candidate[:years_of_experience] >= 2
+end
+
+def git_hub_master?(candidate)
+	candidate[:github_points] >= 100
+end
+
+def ruby_python_master?(candidate)
+	candidate[:languages].detect { |language| language.downcase == 'ruby' or language.downcase == 'python' }
+end
+
+def applied_last_15?(candidate)
+	Date.today - candidate[:date_applied] <= 15
+end
+
+def over_17?(candidate)
+	candidate[:age] > 17
+end
+
+def qualified?(candidate)
+	experienced?(candidate) and	git_hub_master?(candidate) and ruby_python_master?(candidate) and applied_last_15?(candidate) and over_17?(candidate)
 end
 
 # More methods will go below
+def qualified_candidates(candidates)
+	candidates.select do |candidate|
+		qualified?(candidate)
+	end
+end
+
+def ordered_by_qualifications(candidates)
+	candidates.sort_by do |candidate|
+		[candidate[:years_of_experience], candidate[:github_points]]
+	end.reverse
+end
--- candidates/main.rb
+++ candidates/main.rb
@@ -7,6 +7,59 @@ require './filters'
 
 ## Your test code can go here
 
-# binding.pry
+def get_input
+	puts "Please put an input."
+	gets.strip
+end
 
-pp qualified_candidates
+def greenify(string)
+	"\e[32m#{string}\e[0m"
+end
+
+def redify(string)
+	"\e[31m#{string}\e[0m"
+end
+
+def format_candidate(candidate)
+	"#{candidate[:id]} | #{candidate[:years_of_experience]} | #{candidate[:github_points]} | #{candidate[:languages]} | #{candidate[:date_applied]} | #{candidate[:age]}"
+end
+
+def format_candidates(candidates)
+	result = ""
+	candidates.each do |candidate|
+		result += "#{candidate[:id]} | #{candidate[:years_of_experience]} | #{candidate[:github_points]} | #{candidate[:languages]} | #{candidate[:date_applied]} | #{candidate[:age]}\n"
+	end
+	result
+end
+
+def main
+	#binding.pry
+	while true do
+		input = get_input
+		if /find (?<id>[\d+])/ =~ input
+			candidate = find(id.to_i)
+			if candidate
+				puts "id  | Years of experience | Github Points | Languages                    | Date Applied       | Age"
+				qualified?(candidate) ? (puts greenify(format_candidate(candidate))) : (puts redify(format_candidate(candidate)))
+				puts "\n"
+			else
+				puts "No such candidate."
+				puts "\n"
+			end
+		elsif 'all' == input
+			puts "id  | Years of experience | Github Points | Languages | Date Applied | Age"
+			@candidates.each do |candidate|
+				qualified?(candidate) ? (puts greenify(format_candidate(candidate))) : (puts redify(format_candidate(candidate)))
+			end
+			puts "\n"
+		elsif 'qualified' == input
+			puts "id  | Years of experience | Github Points | Languages                    | Date Applied       | Age"
+			puts greenify(format_candidates(ordered_by_qualifications(qualified_candidates(@candidates))))
+		elsif 'quit' == input
+			puts "Exiting out of program."
+			return
+		end
+	end
+end
+
+main
